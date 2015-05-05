70353fddea9399b003606f1cf20741dbf72eec5f
ibakyrdjiev <ibakyrdjiev@gmail.com>
2015-05-05 20:04:21 +0300

    Unit test attached

---
+++ UnitTests/ConsoleReaderTest.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BullsAndCows.InputReaders;
+using BullsAndCows.Interfaces;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BullsAndCowsTest
+{
+    [TestClass]
+    public class ConsoleReaderTest
+    {
+        [TestMethod]
+        public void TestReadInput()
+        {
+            IInputReader reader = new ConsoleReader();
+            string expected = "test";
+
+            using (StringReader sr = new StringReader(expected))
+            {
+                Console.SetIn(sr);
+                Assert.AreEqual(reader.ReadInput(), expected, "The read value should be the same as the input");
+            }
+        }
+    }
+}
---
+++ UnitTests/PlayerInfoTest.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using BullsAndCows.Constants;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BullsAndCowsTest
+{
+    [TestClass]
+    public class PlayerInfoTest
+    {
+        [TestMethod]
+        public void TestCreatePlayerInfo()
+        {
+            PlayerInfo info = new PlayerInfo("nick", 3);
+            Assert.IsInstanceOfType(info, typeof(PlayerInfo), "Should return instance of PlayerInfo");
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException), "ArgumentOutOfRangeException expected")]
+        public void TestPlayerInfoWithNegativeGuesses()
+        {
+            PlayerInfo info = new PlayerInfo("nick", -3);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException), "ArgumentException expected")]
+        public void TestPlayerInfoWithEmptyStringNick()
+        {
+            PlayerInfo info = new PlayerInfo("", 3);
+        }
+
+        [TestMethod]
+        public void TestPlayerInfoToString()
+        {
+            PlayerInfo info = new PlayerInfo("nickname", 3);
+            var expected = String.Format(GameConstants.PlayerInfo, info.Guesses, info.NickName);
+            Assert.AreEqual(info.ToString(), expected);
+        }
+
+        [TestMethod]
+        public void TestPlayerInfoCompareWithDifferentGuessCount()
+        {
+            PlayerInfo firstPlayer = new PlayerInfo("nickname", 3);
+            PlayerInfo secondPlayer = new PlayerInfo("nick", 2);
+            Assert.IsTrue(firstPlayer.CompareTo(secondPlayer) > 0,
+                "Player with less guesses should be first");
+        }
+
+        [TestMethod]
+        public void TestPlayerInfoCompareWithEqualGuessCount()
+        {
+            PlayerInfo firstPlayer = new PlayerInfo("nickname", 2);
+            PlayerInfo secondPlayer = new PlayerInfo("nick", 2);
+            Assert.IsTrue(firstPlayer.CompareTo(secondPlayer) > 0,
+                "When players have equal guesses they should be ordered by their nicknames");
+        }
+    }
+}
